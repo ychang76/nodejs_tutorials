acfb72a5af8d28b2d7c67b4fbd6470ab8c278f9c
Chang <ychang76@gmail.com>
2017-12-08 16:35:51 +0900

    Initialize

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('public'));
+app.get('/template', function(req, res){
+  res.render('temp', {time: Date(), _title: 'Jade'});
+})
+app.get('/', function(req, res){
+  res.send('Hello home page');
+});
+app.get('/dynamic', function(req, res){
+  var lis = '';
+  for(var i = 0; i < 5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  var time = Date();
+  var output = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+        Hello, Dynamic!
+        <ul>
+          ${lis}
+        </ul>
+        ${time}
+    </body>
+  </html>`;
+  res.send(output);
+})
+app.get('/route', function(req, res){
+  res.send('Hello Router, <img src="/route.png">');
+});
+app.get('/login', function(req, res){
+  res.send('<h1>Login please</h1>');
+});
+app.listen(3000, function(){
+  console.log('Connected 3000 port!');
+});
