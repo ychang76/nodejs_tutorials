921c396e90f6c07600c6cdb28ccb42efe20b7a7f
Chang <ychang76@gmail.com>
2017-12-10 00:44:34 +0900

    exercise finished

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/form', function(req, res){
+  res.render('form');
+});
+app.get('/form_receiver', function(req, res){
+  var title = req.query.title;
+  var description = req.query.description;
+  res.send(title+', '+description);
+});
+app.post('/form_receiver', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(title+', '+description);
+});
+app.get('/topic', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic?id=0">JavaScript</a><br>
+  <a href="/topic?id=1">Nodejs</a><br>
+  <a href="/topic?id=2">Express</a><br><br>
+  ${topics[req.query.id]}
+  `
+  res.send(output);
+});
+app.get('/topic/:id', function(req, res){
+  var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic/0">JavaScript</a><br>
+  <a href="/topic/1">Nodejs</a><br>
+  <a href="/topic/2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `
+  res.send(output);
+});
+app.get('/topic/:id/:mode', function(req, res){
+  res.send(req.params.id+','+req.params.mode)
+});
 app.get('/template', function(req, res){
   res.render('temp', {time: Date(), _title: 'Jade'});
-})
+});
 app.get('/', function(req, res){
   res.send('Hello home page');
 });
@@ -32,7 +78,7 @@ app.get('/dynamic', function(req, res){
     </body>
   </html>`;
   res.send(output);
-})
+});
 app.get('/route', function(req, res){
   res.send('Hello Router, <img src="/route.png">');
 });
---
+++ app_file.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var fs = require('fs');
+var app = express();
+app.locals.pretty = true
+app.set('views', './views_file');
+app.set('view engine', 'pug');
+app.get('/upload', function(req, res){
+	res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+	res.send('Uploaded: '  + req.file.filename);
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/topic/new', function(req, res){
+	fs.readdir('data', function(err, files){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		res.render('new', {topics: files});
+	});
+});
+app.get(['/topic', '/topic/:id'], function(req, res){
+	fs.readdir('data', function(err, files){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		var id = req.params.id;
+		if(id){
+			fs.readFile('data/' + id, 'utf8', function(err, data){
+				if(err){
+					console.log(err);
+					res.status(500).send('Internal Server Error');
+				}
+				res.render('view', {topics: files, title: id, description: data});
+			});
+		} else {
+			res.render('view', {topics: files, title: 'Welcome', description: 'Node.js Tutorials'});			
+		}
+	});
+});
+// app.get('/topic/:id', function(req, res){
+// 	var id = req.params.id;
+// 	fs.readdir('data', function(err, files){
+// 		if(err){
+// 			console.log(err);
+// 			res.status(500).send('Internal Server Error');
+// 		}
+// 		fs.readFile('data/' + id, 'utf8', function(err, data){
+// 			if(err){
+// 				console.log(err);
+// 				res.status(500).send('Internal Server Error');
+// 			}
+// 			res.render('view', {topics: files, title: id, description: data});
+// 		});
+// 	});
+// });
+app.post('/topic', function(req, res){
+	var title = req.body.title;
+	var description = req.body.description;
+	fs.writeFile('data/' + title, description, function(err){
+		if(err){
+			console.log(err);
+			res.status(500).send('Internal Server Error');
+		}
+		res.redirect('/topic/' + title);
+	});
+})
+app.listen(3000, function(){
+	console.log('Connected, 3000 port!');
+});
