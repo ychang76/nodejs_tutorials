eaa0abc83c6793cfb1750c526216616a13bb246a
Chang <ychang76@gmail.com>
2017-12-11 01:34:22 +0900

    multer advanced

--- app_file.js
+++ app_file.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'});
+var _storage = multer.diskStorage({
+ 	destination: function (req, file, cb) {
+    		cb(null, 'uploads/')
+  	},
+  	filename: function (req, file, cb) {
+    		cb(null, file.originalname);
+  	}
+});
+var upload = multer({ storage: _storage });
 var fs = require('fs');
 var app = express();
 app.locals.pretty = true
